54760cd6a51e11a1640ff4ff435fddb3462f92c7
Carson Boyne <carson@Carsons-MacBook-Pro3.local>
2024-03-29 14:27:54 -0400

    fixed bugs

---
+++ Lab7/16.8/README.md
@@ -0,0 +1,3 @@
+This folder includes a simple python program which acts as a web server.
+run the script using python3 main.py
+once it's running you can view the html page at http://localhost:8000
--- Lab7/16.8/main.py
+++ Lab7/16.8/main.py
@@ -3,7 +3,8 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 class AjaxRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
-        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
         SimpleHTTPRequestHandler.end_headers(self)
 
 def run(server_class=HTTPServer, handler_class=AjaxRequestHandler, addr="localhost", port=8000):
